69933298714a6519abccccc558ac6cd300b16590
Julio <julio.vitorino.email@gmail.com>
2021-06-26 14:38:20 -0300

    ajustes finais

--- php/classes/campanha/clientAdicionarMaisCarimbosCampanha.php
+++ php/classes/campanha/clientAdicionarMaisCarimbosCampanha.php
@@ -2,6 +2,7 @@
 
 //http://localhost/cfdi/php/classes/campanha/clientAdicionarMaisCarimbosCampanha.php?id_user=1006&id_camp=1004
 //http://elitefinanceira.com/producao/cfdi/php/classes/campanha/clientAdicionarMaisCarimbosCampanha.php?id_user=1000&id_camp=1000
+//http://elitefinanceira.com/cfdi/php/classes/campanha/clientAdicionarMaisCarimbosCampanha.php?id_user=1000&id_camp=1041
 
 require_once './campanhaServiceImpl.php';
 
