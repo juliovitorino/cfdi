7c33729dc6e04d5a8819ff25695c659166d69030
Julio <julio.vitorino.email@gmail.com>
2021-07-08 11:25:42 -0300

    ajustes de banco de dados em ambiente hmg

--- scriptbd/drop-all.sql
+++ scriptbd/drop-all.sql
@@ -9,28 +9,28 @@ WHERE TABLE_SCHEMA = 'cfd'
 AND CONSTRAINT_NAME LIKE 'FK_%';
 
 /* EXECUTE A SQL ABAIXO */
-select concat('ALTER TABLE ',TABLE_NAME,' DROP FOREIGN KEY ', CONSTRAINT_NAME, ';')
+select concat('ALTER TABLE ',upper(TABLE_NAME),' DROP FOREIGN KEY ', upper(CONSTRAINT_NAME), ';')
 from information_schema.KEY_COLUMN_USAGE
 WHERE TABLE_SCHEMA = 'cfd'
 AND CONSTRAINT_NAME LIKE 'FK_%'
 UNION ALL
-select concat('ALTER TABLE ',TABLE_NAME,' DROP INDEX ', CONSTRAINT_NAME, ';')
+select concat('ALTER TABLE ',upper(TABLE_NAME),' DROP INDEX ', upper(CONSTRAINT_NAME), ';')
 from information_schema.KEY_COLUMN_USAGE
 WHERE TABLE_SCHEMA = 'cfd'
 AND CONSTRAINT_NAME LIKE 'UN_%'
 UNION ALL
-select concat('ALTER TABLE ',TABLE_NAME,' DROP INDEX ', CONSTRAINT_NAME, ';')
+select concat('ALTER TABLE ',upper(TABLE_NAME),' DROP INDEX ', upper(CONSTRAINT_NAME), ';')
 from information_schema.KEY_COLUMN_USAGE
 WHERE TABLE_SCHEMA = 'cfd'
 AND CONSTRAINT_NAME LIKE 'UIX_%'
 UNION ALL
-select concat('ALTER TABLE ',TABLE_NAME,' DROP INDEX ', CONSTRAINT_NAME, ';')
+select concat('ALTER TABLE ',upper(TABLE_NAME),' DROP INDEX ', upper(CONSTRAINT_NAME), ';')
 from information_schema.KEY_COLUMN_USAGE
 WHERE TABLE_SCHEMA = 'cfd'
 AND CONSTRAINT_NAME LIKE 'IX_%'
 UNION ALL
-SELECT concat('DROP TABLE ', Table_name, ';')
-from information_schema. tables where table_schema = 'cfd'
+SELECT concat('DROP TABLE ', upper(Table_name), ';')
+from information_schema.tables where table_schema = 'cfd'
 INTO OUTFILE '/temp/drop-all-commands.txt';
 
 
