1298d4de72014b9a85d641cf81434b324eca029b
Julio <julio.vitorino.email@gmail.com>
2021-07-08 10:05:44 -0300

    ajuste de query de apoio saida pra arquivo

--- scriptbd/drop-all.sql
+++ scriptbd/drop-all.sql
@@ -30,12 +30,13 @@ WHERE TABLE_SCHEMA = 'cfd'
 AND CONSTRAINT_NAME LIKE 'IX_%'
 UNION ALL
 SELECT concat('DROP TABLE ', Table_name, ';')
-from information_schema. tables where table_schema = 'cfd';
+from information_schema. tables where table_schema = 'cfd'
+INTO OUTFILE '/temp/drop-all-commands.txt';
 
 
 /* desabilita todas FKs para efetivar o DROP TABLE */
 SET FOREIGN_KEY_CHECKS=0;
 
 /* COLE AQUI O RESULTADO DA QUERY ACIMA */
- 
+
 SET FOREIGN_KEY_CHECKS=1;
