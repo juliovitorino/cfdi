bc316b90c83dfd5569020e39eb20d31441ba3064
Julio <julio.vitorino.email@gmail.com>
2021-07-10 11:32:23 -0300

    ajuste de client de acerto de saldo

--- php/classes/campanhacashbackcc/clientRegistrarCashbackCC.php
+++ php/classes/campanhacashbackcc/clientRegistrarCashbackCC.php
@@ -1,12 +1,13 @@
 <?php 
 
 // URL http://localhost/cfdi/php/classes/campanhacashbackcc/clientRegistrarCashbackCC.php
+// URL http://elitefinanceira.com/producao/cfdi/php/classes/campanhacashbackcc/clientRegistrarCashbackCC.php?usua_id=1000
 
 require_once 'CampanhaCashbackCCServiceImpl.php';
 
 
 $csi = new CampanhaCashbackCCServiceImpl();
-$id_usuario = 1003;
+$id_usuario = (int) $_GET['usua_id'];
 
 $retorno = $csi->registrarSaldoCashbackCC($id_usuario);
 var_dump($retorno);
